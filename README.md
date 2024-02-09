84dbfc79fcdfe459314c8cb10ee10f9580fa0a37
maurogallotta <maurogallotta@maurogallotta.com>
2024-02-09 10:17:50 -0300

    meu commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h2 align="center"> Bootcamp Potência Tech iFood - Desenvolvimento de Jogos </h2>
+
+<h3 align="center"> Desafio do Projeto - Criando um Jogo da Memória com Javascript </h3>
+
+<a href="https://alderj.github.io/jogo-da-memoria-com-emojis/"> <img src="https://github.com/Alderj/jogo-da-memoria-com-emojis/blob/main/src/jogo%20da%20memoria.png?raw=true" /> </a>
---
+++ src/scripts/engine.js
@@ -0,0 +1,61 @@
+const emojis = [
+    "🍕",
+    "🍕",
+    "🍔",
+    "🍔",
+    "🍟",
+    "🍟",
+    "🌭",
+    "🌭",
+    "🍿",
+    "🍿",
+    "🥚",
+    "🥚",
+    "🥓",
+    "🥓",
+    "🥨",
+    "🥨",
+];
+let openCards = [];
+
+let shuffeEmojis = emojis.sort(()=>(Math.random() > 0.5 ? 2 : -1));
+
+for(let i=0; i < emojis.length; i++){
+
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffeEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box);
+
+}
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if (openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+};
+
+function checkMatch(){
+
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }else{
+        openCards[0].classList .remove("boxOpen");
+        openCards[1].classList .remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length===emojis.length){
+        alert("Voce ganhou!!!!")
+    }
+
+}
+
